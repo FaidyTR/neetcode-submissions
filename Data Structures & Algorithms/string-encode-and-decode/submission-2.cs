e4614832eb53001911cb52d984ae196aa8704573
Faidy Rhaiem <faidyrhaiem2@gmail.com>
2026-07-28 21:19:58 +0100

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.cs
@@ -0,0 +1,28 @@
+public class Solution {
+
+    public string Encode(IList<string> strs) {
+        string encode = "";
+        string ch;
+        foreach (string s in strs)
+        {
+            ch = s + "/";
+            encode += ch;
+        }
+        return encode;
+    }
+
+    public List<string> Decode(string s) {
+        List<string> decodeList = new List<string>();
+        int j = 0;
+        for(int i = 0; i < s.Length; i++)
+        {
+            if (s[i] == '/')
+            {
+                decodeList.Add(s.Substring(j, i - j));
+                j = i + 1;
+            }
+        }
+
+        return decodeList;
+   }
+}
