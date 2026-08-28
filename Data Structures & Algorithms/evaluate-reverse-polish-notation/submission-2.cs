214141976398831ec728f0d441085cacccade04e
Faidy Rhaiem <faidyrhaiem2@gmail.com>
2026-08-28 17:07:51 +0100

    Add: evaluate-reverse-polish-notation - submission-2

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-2.cs
@@ -0,0 +1,36 @@
+public class Solution {
+    public int EvalRPN(string[] tokens) {
+        Stack<int> stack = new Stack<int>(); 
+
+        foreach (string token in tokens)
+        {
+            if (!IsOperator(token))
+            {
+                stack.Push(int.Parse(token));
+            }
+            else
+            {
+                switch (token)
+                {
+                    case "+":
+                        stack.Push(stack.Pop() + stack.Pop());
+                        break;
+                    case "*":
+                        stack.Push(stack.Pop() * stack.Pop());
+                        break;
+                    case "/":
+                        stack.Push(stack.Pop() / stack.Pop());
+                        break;
+                    case "-":
+                        stack.Push(-stack.Pop() + stack.Pop());
+                        break;
+                }
+            }
+        }
+        return stack.Pop();
+    }
+    private bool IsOperator(string c)
+    {
+        return c == "+" || c == "*" || c == "-" || c == "/";
+    }   
+}
